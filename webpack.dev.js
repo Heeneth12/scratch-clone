bd29373df77d2f3a05761c71ed6a9a18438c4136
Heeneth sai <114326514+Heeneth12@users.noreply.github.com>
2025-05-05 13:15:37 +0530

    Update webpack.dev.js
    fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,15 +2,17 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge([
-  common,
-  {
-    mode: "development",
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      compress: true,
-      port: 3000,
-      historyApiFallback: true,
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
     },
+    compress: true,
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
+    historyApiFallback: true,
+    hot: true,
+    open: false,
   },
-]);
+});
